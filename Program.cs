d471568eef39b0ca1d02b20b72b174a9cd6398f3
Mirzomiddin <141604581+MirzakhanDeveloper@users.noreply.github.com>
2023-08-08 23:57:42 +0500

    INFRA : Upload Project

--- Program.cs
+++ Program.cs
@@ -1,8 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-string a = "";
-do 
-{
-    Console.WriteLine("Enter password :");
-    a = Console.ReadLine();
-}while( a != "123");
+﻿
+string[] fruits = new string[3];
+fruits[0] = Console.ReadLine();
+fruits[1] = Console.ReadLine();
+fruits[2] = Console.ReadLine();
+Console.WriteLine(fruits[0] +" "+ fruits[1] +" "+ fruits[2]);
+int[] id = {1,2,3,4};
+Console.WriteLine($"{id[0]}");
+string[] name = {"Jane","Kain","Barter","Jarry",};
+int a = id[0]+id[1];
+string b = name[0]+name[1];
+Console.WriteLine(a + b);
 
