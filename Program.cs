5f723b6b07a6d9aa3905897e33bfe2cd939200e9
Mirzomiddin <141604581+MirzakhanDeveloper@users.noreply.github.com>
2023-08-08 20:41:19 +0500

    INFRA : Upload Project

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Bek");
+Console.Write("Enter number: ");
+int a = 0;
+a = Convert.ToInt32(Console.ReadLine());
+if ( a > 0 )
+{
+    int b = 0;
+    while ( b < a )
+    {
+        Console.WriteLine( b % 2 == 0 ? $"Juft {b}" : "" );
+        Console.WriteLine( b % 2 == 1 ? $"Toq  {b}" : "" );
+        b ++;    
+        
+    }
+}
 
