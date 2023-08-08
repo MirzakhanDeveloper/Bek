1d5b4cf45c4e0783ede8140f8f6e75ffc04636ad
Mirzomiddin <141604581+MirzakhanDeveloper@users.noreply.github.com>
2023-08-08 23:26:47 +0500

    INFRA : Upload project

--- Program.cs
+++ Program.cs
@@ -1,16 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.Write("Enter number: ");
-int a = 0;
-a = Convert.ToInt32(Console.ReadLine());
-if ( a > 0 )
+string a = "";
+do 
 {
-    int b = 0;
-    while ( b < a )
-    {
-        Console.WriteLine( b % 2 == 0 ? $"Juft {b}" : "" );
-        Console.WriteLine( b % 2 == 1 ? $"Toq  {b}" : "" );
-        b ++;    
-        
-    }
-}
+    Console.WriteLine("Enter password :");
+    a = Console.ReadLine();
+}while( a != "123");
 
