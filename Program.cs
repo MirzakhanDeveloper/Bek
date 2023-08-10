0c0c78b768e410eaeaec2f3b5244a0f0ee35af03
Mirzomiddin <141604581+MirzakhanDeveloper@users.noreply.github.com>
2023-08-10 09:21:18 +0500

    INFRA : Upload Project

--- Program.cs
+++ Program.cs
@@ -1,9 +1,27 @@
 ﻿using BEK.Classes;
 
-Fruit apple = new Fruit();
-apple.price = 120;
-apple.name = "Qizil";
-apple.quantity = 40;
+Fruit apple = new ()
+{
+    price = 120,
+    name = "Qizil",
+    quantity = 40
+};
+
+
+
+
+
+Fruit piece = new ()
+{
+    price = 0,
+    name = "s",
+    quantity = 0    
+};
+
+Console.WriteLine($"Piece - {piece.price}-{piece.name}-{piece.quantity}");
+
+
+
 Console.WriteLine($"Name (Fruit): {apple.name}\n Quantity : {apple.quantity} \n Price :{apple.price} \n ----------");
 
 apple.Price = 7500;
@@ -16,11 +34,12 @@ Console.WriteLine($"Field (quantity) - {apple.quantity}");
 
 Console.WriteLine($"Property (Quantity) - {apple.Quantity} \n --------------");
 
-Fruit pomadoro = new Fruit();
-pomadoro.Name = "Pomadoro";
-pomadoro.Price = 4000;
-pomadoro.Quantity = 23;
-
+Fruit pomadoro = new ()
+{
+     Price = 4000,
+     Name = "Pomadoro",
+     Quantity = 23
+};
 Console.WriteLine($"Name (Fruit): {pomadoro.Name} \n Price: {pomadoro.Price} \n Quantity: {pomadoro.Quantity}\n ----------");
 
 
@@ -28,30 +47,36 @@ Console.WriteLine($"Name (Fruit): {pomadoro.Name} \n Price: {pomadoro.Price} \n 
 
 
 
-Vegetable carrot = new Vegetable();
-carrot.Name = "Carrot";
-carrot.Price = 1800;
-carrot.Quantity = 52;
-
+Vegetable carrot = new ()
+{
+    Name = "Carrot",
+    Price = 1800,
+    Quantity = 52
+};
 Console.WriteLine($"Name (Vegetable): {carrot.Name} \n Price: {carrot.Price} \n Quantity: {carrot.Quantity}\n----------");
 
 
 
-Flower flower = new Flower();
+Flower flower = new ()
+{
+    Name = "Rose",
+    Price = 15000,
+    Quantity = 70,
+    Place ="Ferghana"
+};
 
-flower.Name = "Rose";
-flower.Price = 15000;
-flower.Quantity = 70;
-flower.Place ="Ferghana";
-
-Animal bear = new Animal();
-bear.Name = "Panda";
-bear.Quantity = 0;
-bear.Place = "Forest";
-bear.Age = 5;
+Animal bear = new ()
+{
+    Name = "Panda",
+    Quantity = 0,
+    Place = "Forest",
+    Age = 5
+};
 
 Console.WriteLine($"Name (Flower) : {flower.Name} \n Price : {flower.Price} \n Quantity : {flower.Quantity} \n Place :{flower.Place}\n-------");
 Console.WriteLine($"Name (Animal) : {bear.Name} \n Quantity : {bear.Quantity} \n Place :{bear.Place} \n Age :{bear.Age}");
+
+
 
 
 
